989cb774c69211e454c4de545dad3494a63e111b
Derek Sewell <dereksewell@directways.co.uk>
2016-01-12 16:00:51 +0000

    inital commit

---
+++ desktop/src/tech/derek/rpgsix/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tech.derek.rpgsix.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tech.derek.rpgsix.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
---
+++ html/src/tech/derek/rpgsix/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package tech.derek.rpgsix.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import tech.derek.rpgsix.Core;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Core();
+        }
+}
